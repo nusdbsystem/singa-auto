0af38854f4c5b727062a261c31419cc7995d736a
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2020-03-07 14:39:35 +0800

    [web] TrialsDetail separate getPlotDetails

--- web/src/containers/Jobs/TrialsDetails.js
+++ web/src/containers/Jobs/TrialsDetails.js
@@ -22,8 +22,11 @@ import { compose } from "redux"
 import { Route, withRouter } from "react-router-dom"
 import { withStyles } from "@material-ui/core/styles"
 import * as moment from "moment"
+
 import PlotManager from "app/PlotManager"
 import RafikiClient from "app/RafikiClient"
+import getPlotDetails from "app/getPlotDetails"
+
 import HTTPconfig from "HTTPconfig"
 
 import {
@@ -307,44 +310,6 @@ class TrialDetails extends React.Component {
   }
 }
 
-function getPlotDetails(plot, metrics) {
-  const seriesByName = {}
-  for (const plotMetric of plot.metrics) {
-    seriesByName[plotMetric] = {
-      data: [],
-      name: plotMetric,
-    }
-  }
-  const xAxis = plot.x_axis || "time"
-
-  for (const metric of metrics) {
-    // Check if x axis value exists
-    if (!(xAxis in metric)) {
-      continue
-    }
-
-    // For each of plot's y axis metrics, push the [x, y] to data array
-    for (const plotMetric of plot.metrics) {
-      if (!(plotMetric in metric)) {
-        continue
-      }
-
-      // Push x axis value to data array
-      seriesByName[plotMetric].data.push([metric[xAxis], metric[plotMetric]])
-    }
-  }
-
-  const plotOption = {
-    title: plot.title,
-    xAxis: {
-      // eslint-disable-next-line
-      type: xAxis == "time" ? "time" : "number",
-      name: xAxis,
-    },
-  }
-
-  return { series: Object.values(seriesByName), plotOption }
-}
 
 export default compose(
   withRouter,
