3eee4cabba95cc759834e921ac2ff44e76da3667
pinpom <thao.nguyen038@gmail.com>
2020-04-06 16:08:20 +0800

    K-Nearest Neighbors Classifier

---
+++ examples/datasets/tabular/load_heart.py
@@ -0,0 +1,19 @@
+from examples.datasets.tabular.csv_file import load
+
+# Loads the "Heart Disease UCI" CSV dataset from kaggle for the `TABULAR_REGRESSION` task
+# (
+# step 1: Install Kaggle API. 
+# step2: From Rafiki root folder, run: `kaggle datasets download ronitf/heart-disease-uci -p data --unzip` 
+# to download the `heart.csv` file to `rafiki/data` folder.
+#)
+
+def load_heart():
+    load(
+        dataset_url='data/heart.csv',
+        out_train_dataset_path='data/heart_train.csv',
+        out_val_dataset_path='data/heart_val.csv'
+    )
+
+
+if __name__ == '__main__':
+    load_heart()    
