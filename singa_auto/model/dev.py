faa96d827a53a7c100558893efe17dfd2ffa519a
delphieritas <34205896+delphieritas@users.noreply.github.com>
2020-05-22 16:12:38 +0800

    dev.py for question_answering task compatibility

--- singa_auto/model/dev.py
+++ singa_auto/model/dev.py
@@ -1,3 +1,4 @@
+
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -34,9 +35,15 @@ from singa_auto.redis import ParamCache, TrainCache, InferenceCache
 from .model import BaseModel, BaseKnob, Params
 from .utils import serialize_knob_config, deserialize_knob_config, parse_model_install_command, load_model_class
 
-def tune_model(py_model_class: Type[BaseModel], train_dataset_path: str, val_dataset_path: str,
-                test_dataset_path: str = None, budget: Budget = None,
-                train_args: Dict[str, any] = None) -> (Dict[str, Any], float, Params):
+
+def tune_model(
+        py_model_class: Type[BaseModel],
+        train_dataset_path: str,
+        val_dataset_path: str,
+        task: str,
+        test_dataset_path: str = None,
+        budget: Budget = None,
+        train_args: Dict[str, any] = None) -> (Dict[str, Any], float, Params):
 
     worker_id = 'local'
 
@@ -95,7 +102,7 @@ def tune_model(py_model_class: Type[BaseModel], train_dataset_path: str, val_dat
         if proposal is None:
             print('No more proposals from advisor - to stop training')
             break
-        proposal.knobs = { **proposal.knobs, **knobs_from_args }
+        proposal.knobs = {**proposal.knobs, **knobs_from_args}
         train_cache.create_proposal(worker_id, proposal)
 
         # Worker receives proposal
@@ -114,17 +121,22 @@ def tune_model(py_model_class: Type[BaseModel], train_dataset_path: str, val_dat
 
         # Worker trains model
         print('Training model...')
-        model_inst.train(train_dataset_path, shared_params=shared_params, **(train_args or {}))
+        model_inst.train(train_dataset_path,
+                         shared_params=shared_params,
+                         **(train_args or {}))
 
         # Worker evaluates model
         result = _evaluate_model(model_inst, proposal, val_dataset_path)
 
         # Worker caches/saves model parameters
-        store_params_id = _save_model(model_inst, proposal, result, param_cache, param_store)
+        store_params_id = _save_model(model_inst, proposal, result, param_cache,
+                                      param_store)
 
         # Update best saved model
         if result.score is not None and store_params_id is not None and result.score > best_model_score:
-            inform_user('Best saved model so far! Beats previous best of score {}!'.format(best_model_score))
+            inform_user(
+                'Best saved model so far! Beats previous best of score {}!'.
+                format(best_model_score))
             best_store_params_id = store_params_id
             best_proposal = proposal
             best_model_score = result.score
@@ -134,7 +146,8 @@ def tune_model(py_model_class: Type[BaseModel], train_dataset_path: str, val_dat
             if test_dataset_path is not None:
                 print('Evaluating new best model on test dataset...')
                 best_model_test_score = model_inst.evaluate(test_dataset_path)
-                inform_user('Score on test dataset: {}'.format(best_model_test_score))
+                inform_user(
+                    'Score on test dataset: {}'.format(best_model_test_score))
 
         # Worker sends result to advisor
         print('Giving feedback to advisor...')
@@ -150,14 +163,19 @@ def tune_model(py_model_class: Type[BaseModel], train_dataset_path: str, val_dat
         # Destroy model
         model_inst.destroy()
 
+        if task == 'question_answering':
+            break
+
     # Train worker tells advisor that it is no longer free
     train_cache.delete_worker(worker_id)
 
     # Declare best model
     if best_proposal is not None:
-        inform_user('Best trial #{} has knobs {} with score of {}'.format(best_trial_no, best_proposal.knobs, best_model_score))
+        inform_user('Best trial #{} has knobs {} with score of {}'.format(
+            best_trial_no, best_proposal.knobs, best_model_score))
         if best_model_test_score is not None:
-            inform_user('...with test score of {}'.format(best_model_test_score))
+            inform_user(
+                '...with test score of {}'.format(best_model_test_score))
 
     # Load params for best model
     best_params = None
@@ -174,7 +192,10 @@ def tune_model(py_model_class: Type[BaseModel], train_dataset_path: str, val_dat
 
     return (best_proposal, best_model_test_score, best_params)
 
-def make_predictions(queries: List[Any], task: str, py_model_class: Type[BaseModel], proposal: Proposal, params: Params) -> List[Any]:
+
+def make_predictions(queries: List[Any], task: str,
+                     py_model_class: Type[BaseModel], proposal: Proposal,
+                     params: Params) -> List[Any]:
     inference_cache: InferenceCache = InferenceCache()
     worker_id = 'local'
 
@@ -200,13 +221,16 @@ def make_predictions(queries: List[Any], task: str, py_model_class: Type[BaseMod
     inference_cache.add_queries_for_worker(worker_id, queries)
 
     # Worker receives query
-    queries_at_worker = inference_cache.pop_queries_for_worker(worker_id, len(queries))
+    queries_at_worker = inference_cache.pop_queries_for_worker(
+        worker_id, len(queries))
     assert len(queries_at_worker) == len(queries)
 
     # Worker makes prediction on queries
     _print_header('Making predictions with trained model...')
     predictions = model_inst.predict([x.query for x in queries_at_worker])
-    predictions = [Prediction(x, query.id, worker_id) for (x, query) in zip(predictions, queries_at_worker)]
+    predictions = [Prediction(x, query.id, worker_id)
+        for (x, query) in zip(predictions, queries_at_worker)
+    ]
 
     # Worker sends predictions to predictor
     inference_cache.add_predictions_for_worker(worker_id, predictions)
@@ -214,28 +238,42 @@ def make_predictions(queries: List[Any], task: str, py_model_class: Type[BaseMod
     # Predictor receives predictions
     predictions_at_predictor = []
     for query in queries:
-        prediction = inference_cache.take_prediction_for_worker(worker_id, query.id)
+        prediction = inference_cache.take_prediction_for_worker(
+            worker_id, query.id)
         assert prediction is not None
         predictions_at_predictor.append(prediction)
 
     # Predictor ensembles predictions
-    ensemble_method = get_ensemble_method(task)
-    print(f'Ensemble method: {ensemble_method}')
-    out_predictions = []
-    for prediction in predictions_at_predictor:
-        prediction = prediction.prediction
-        _assert_jsonable(prediction, Exception('Each `prediction` should be JSON serializable'))
-        out_prediction = ensemble_method([prediction])
-        out_predictions.append(out_prediction)
+    if task == 'question_answering':
+        return (predictions_at_predictor[0].__dict__, model_inst)
+    else:
+        ensemble_method = get_ensemble_method(task)
+        print(f'Ensemble method: {ensemble_method}')
+        out_predictions = []
+        for prediction in predictions_at_predictor:
+            prediction = prediction.prediction
+            _assert_jsonable(
+                prediction,
+                Exception('Each `prediction` should be JSON serializable'))
+            out_prediction = ensemble_method([prediction])
+            out_predictions.append(out_prediction)
 
-    print('Predictions: {}'.format(out_predictions))
+        print('Predictions: {}'.format(out_predictions))
 
-    return (out_predictions, model_inst)
+        return (out_predictions, model_inst)
+
 
 # TODO: Fix method, more thorough testing of model API
-def test_model_class(model_file_path: str, model_class: str, task: str, dependencies: Dict[str, str],
-                    train_dataset_path: str, val_dataset_path: str, test_dataset_path: str = None,
-                    budget: Budget = None, train_args: Dict[str, any] = None, queries: List[Any] = None) -> (List[Any], BaseModel):
+def test_model_class(model_file_path: str,
+                     model_class: str,
+                     task: str,
+                     dependencies: Dict[str, str],
+                     train_dataset_path: str,
+                     val_dataset_path: str,
+                     test_dataset_path: str = None,
+                     budget: Budget = None,
+                     train_args: Dict[str, any] = None,
+                     queries: List[Any] = None) -> (List[Any], BaseModel):
     '''
     Tests whether a model class is *more likely* to be correctly defined by *locally* simulating a full train-inference flow on your model
     on a given dataset. The model's methods will be called in an manner similar to that in Rafiki.
@@ -264,19 +302,28 @@ def test_model_class(model_file_path: str, model_class: str, task: str, dependen
     _print_header('Checking loading of model & model definition...')
     with open(model_file_path, 'rb') as f:
         model_file_bytes = f.read()
-    py_model_class = load_model_class(model_file_bytes, model_class, temp_mod_name=model_class)
+    py_model_class = load_model_class(model_file_bytes,
+                                      model_class,
+                                      temp_mod_name=model_class)
     _check_model_class(py_model_class)
 
     # Simulation of training
-    (best_proposal, _, best_params) = tune_model(py_model_class, train_dataset_path, val_dataset_path,
-                                                test_dataset_path=test_dataset_path, budget=budget,
-                                                train_args=train_args)
+    (best_proposal, _,
+     best_params) = tune_model(py_model_class,
+                               train_dataset_path,
+                               val_dataset_path,
+                               task,
+                               test_dataset_path=test_dataset_path,
+                               budget=budget,
+                               train_args=train_args)
 
     # Simulation of inference
     model_inst = None
     predictions = None
     if best_proposal is not None and best_params is not None and queries is not None:
-        (predictions, model_inst) = make_predictions(queries, task, py_model_class, best_proposal, best_params)
+        (predictions, model_inst) = make_predictions(queries, task,
+                                                     py_model_class,
+                                                     best_proposal, best_params)
 
     py_model_class.teardown()
 
@@ -284,11 +331,14 @@ def test_model_class(model_file_path: str, model_class: str, task: str, dependen
 
     return (predictions, model_inst)
 
+
 def warn_user(msg):
     print(f'\033[93mWARNING: {msg}\033[0m')
 
+
 def inform_user(msg):
     print(f'\033[94m{msg}\033[0m')
+
 
 def _pull_shared_params(proposal: Proposal, param_cache: ParamCache):
     if proposal.params_type == ParamsType.NONE:
@@ -297,6 +347,7 @@ def _pull_shared_params(proposal: Proposal, param_cache: ParamCache):
     print('Retrieving shared params from cache...')
     shared_params = param_cache.retrieve_params(proposal.params_type)
     return shared_params
+
 
 def _evaluate_model(model_inst: BaseModel, proposal: Proposal,
                     val_dataset_path: str) -> TrialResult:
@@ -312,6 +363,7 @@ def _evaluate_model(model_inst: BaseModel, proposal: Proposal,
     print('Score on validation dataset:', score)
     return TrialResult(proposal, score=score)
 
+
 def _save_model(model_inst: BaseModel, proposal: Proposal, result: TrialResult,
                 param_cache: ParamCache, param_store: ParamStore):
     if not proposal.to_cache_params and not proposal.to_save_params:
@@ -321,7 +373,9 @@ def _save_model(model_inst: BaseModel, proposal: Proposal, result: TrialResult,
     params = model_inst.dump_parameters()
     if proposal.to_cache_params:
         print('Storing shared params in cache...')
-        param_cache.store_params(params, score=result.score, time=datetime.now())
+        param_cache.store_params(params,
+                                 score=result.score,
+                                 time=datetime.now())
 
     store_params_id = None
     if proposal.to_save_params:
@@ -329,6 +383,7 @@ def _save_model(model_inst: BaseModel, proposal: Proposal, result: TrialResult,
         store_params_id = param_store.save(params)
 
     return store_params_id
+
 
 def _maybe_read_knobs_from_args(knob_config):
     parser = argparse.ArgumentParser()
@@ -347,24 +402,29 @@ def _maybe_read_knobs_from_args(knob_config):
             if knob.value_type in [list, bool]:
                 value = eval(value)
             knobs_from_args[name] = value
-            inform_user('Setting knob "{}" to be fixed value of "{}"...'.format(name, value))
+            inform_user('Setting knob "{}" to be fixed value of "{}"...'.format(
+                name, value))
 
     return knobs_from_args
+
 
 def _maybe_read_budget_from_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--GPU_COUNT', type=int, default=0)
-    parser.add_argument('--TIME_HOURS', type=float, default=0.01) # < 1 min
+    parser.add_argument('--TIME_HOURS', type=float, default=0.01)  # < 1 min
     parser.add_argument('--MODEL_TRIAL_COUNT', type=int, default=-1)
     budget_from_args = vars(parser.parse_known_args()[0])
     return budget_from_args
+
 
 def _check_model_class(py_model_class):
     if not issubclass(py_model_class, BaseModel):
         raise Exception('Model should extend `singa_auto.model.BaseModel`')
 
     if inspect.isfunction(getattr(py_model_class, 'init', None)):
-        warn_user('`init` has been deprecated - use `__init__` for your model\'s initialization logic instead')
+        warn_user(
+            '`init` has been deprecated - use `__init__` for your model\'s initialization logic instead'
+        )
 
     if inspect.isfunction(getattr(py_model_class, 'get_knob_config', None)) and \
         not isinstance(py_model_class.__dict__.get('get_knob_config', None), staticmethod):
@@ -378,7 +438,9 @@ def _check_dependencies(dependencies):
     for (dep, ver) in dependencies.items():
         if dep == ModelDependency.KERAS:
             # Warn that Keras models should additionally depend on TF for GPU usage
-            inform_user('Keras models can enable GPU usage with by adding a `tensorflow` dependency.')
+            inform_user(
+                'Keras models can enable GPU usage with by adding a `tensorflow` dependency.'
+            )
         elif dep in [ModelDependency.TORCH, ModelDependency.TORCHVISION]:
             pass
         elif dep == ModelDependency.SCIKIT_LEARN:
@@ -389,8 +451,10 @@ def _check_dependencies(dependencies):
         elif dep == ModelDependency.SINGA:
             pass
 
-    install_command = parse_model_install_command(dependencies, enable_gpu=False)
-    install_command_with_gpu = parse_model_install_command(dependencies, enable_gpu=True)
+    install_command = parse_model_install_command(dependencies,
+                                                  enable_gpu=False)
+    install_command_with_gpu = parse_model_install_command(dependencies,
+                                                           enable_gpu=True)
     inform_user(f'Install command (without GPU): `{install_command}`')
     inform_user(f'Install command (with GPU): `{install_command_with_gpu}`')
 
@@ -398,11 +462,14 @@ def _check_dependencies(dependencies):
 def _check_knob_config(knob_config):
     if not isinstance(knob_config, dict) or \
         any([(not isinstance(name, str) or not isinstance(knob, BaseKnob)) for (name, knob) in knob_config.items()]):
-        raise Exception('Static method `get_knob_config()` should return a dict[str, BaseKnob]')
+        raise Exception(
+            'Static method `get_knob_config()` should return a dict[str, BaseKnob]'
+        )
 
     # Try serializing and deserialize knob config
     knob_config_bytes = serialize_knob_config(knob_config)
     knob_config = deserialize_knob_config(knob_config_bytes)
+
 
 def _assert_jsonable(jsonable, exception=None):
     try:
@@ -411,26 +478,45 @@ def _assert_jsonable(jsonable, exception=None):
         traceback.print_stack()
         raise exception or e
 
+
 def _check_model_inst(model_inst):
     # Throw error when deprecated methods are called
     def deprecated_func(desc):
+
         def throw_error(*args, **kwargs):
             raise AttributeError(desc)
 
         return throw_error
 
     class DeprecatedModelUtils():
-        log = deprecated_func('`self.utils.log(...)` has been moved to `logger.log(...)`')
-        log_metrics = deprecated_func('`self.utils.log_metrics(...)` has been moved to `logger.log(...)`')
-        define_plot = deprecated_func('`self.utils.define_plot(...)` has been moved to `logger.define_plot(...)`')
-        define_loss_plot = deprecated_func('`self.utils.define_loss_plot(...)` has been moved to `logger.define_loss_plot(...)`')
-        log_loss_metric = deprecated_func('`self.utils.log_loss_metric(...)` has been moved to `logger.log_loss(...)`')
-        load_dataset_of_image_files = deprecated_func('`self.utils.load_dataset_of_image_files(...)` has been moved to `dataset_utils.load_dataset_of_image_files(...)`')
-        load_dataset_of_corpus = deprecated_func('`self.utils.load_dataset_of_corpus(...)` has been moved to `dataset_utils.load_dataset_of_corpus(...)`')
-        resize_as_images = deprecated_func('`self.utils.resize_as_images(...)` has been moved to `dataset_utils.resize_as_images(...)`')
-        download_dataset_from_uri = deprecated_func('`self.utils.download_dataset_from_uri(...)` has been moved to `dataset_utils.download_dataset_from_uri(...)`')
+        log = deprecated_func(
+            '`self.utils.log(...)` has been moved to `logger.log(...)`')
+        log_metrics = deprecated_func(
+            '`self.utils.log_metrics(...)` has been moved to `logger.log(...)`')
+        define_plot = deprecated_func(
+            '`self.utils.define_plot(...)` has been moved to `logger.define_plot(...)`'
+        )
+        define_loss_plot = deprecated_func(
+            '`self.utils.define_loss_plot(...)` has been moved to `logger.define_loss_plot(...)`'
+        )
+        log_loss_metric = deprecated_func(
+            '`self.utils.log_loss_metric(...)` has been moved to `logger.log_loss(...)`'
+        )
+        load_dataset_of_image_files = deprecated_func(
+            '`self.utils.load_dataset_of_image_files(...)` has been moved to `dataset_utils.load_dataset_of_image_files(...)`'
+        )
+        load_dataset_of_corpus = deprecated_func(
+            '`self.utils.load_dataset_of_corpus(...)` has been moved to `dataset_utils.load_dataset_of_corpus(...)`'
+        )
+        resize_as_images = deprecated_func(
+            '`self.utils.resize_as_images(...)` has been moved to `dataset_utils.resize_as_images(...)`'
+        )
+        download_dataset_from_uri = deprecated_func(
+            '`self.utils.download_dataset_from_uri(...)` has been moved to `dataset_utils.download_dataset_from_uri(...)`'
+        )
 
     model_inst.utils = DeprecatedModelUtils()
+
 
 def _print_header(msg):
     print('-' * (len(msg) + 4))
